d452fe1b64cabdd2df6a975be39a860116b65866
ushimasahiro <ushimasahiro@gmail.com>
2025-05-09 14:04:12 +0900

    second
    git push

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 set -o errexit
-pip intall -r requirements.txt
+
+pip install -r requirements.txt
+
 python3 manage.py collectstatic --no-input
 python3 manage.py migrate
 python3 mange.py superuser
